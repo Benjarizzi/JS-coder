6f4564402febf2fbd2b7c0926d5a6a7ae31d4732
Benjarizzi <rizzibenja@gmail.com>
2023-02-16 23:09:13 -0300

    Agregando funciones,objetos y arrays

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,26 @@
-   //Condicional
-let ingresar = prompt("Vas a comprar algo el día de hoy? SI NO");
 
-if (ingresar == "Si" || ingresar == "si" || ingresar == "SI") {
-    alert("Genial!  empezemos!");
-} else {
-    alert(":(")
-} 
-
-//bucle do while
+   //bucle do while
 let contraseña = "computadora"
 do{
     contraseña = prompt("Ingrese su contraseña");
 }
 while(contraseña != "computadora")
+//Array
+const productos = [ "Coca", "Moritas", "Chocolate"]
+if (productos.includes("Coca")){
+    alert(productos.indexOf("Coca"));
+}
+//Objeto
+const producto1 ={
+    tamaño:"Grande",
+    cantidad: "un litro y medio",
+    color:"Negro"
+} 
+alert(producto1.cantidad);
+
+//Función 
+function saludar (nombre){
+    return "hola " + nombre
+}
+let Saludogral = saludar(prompt("Ingrese su nombre"));
+alert(Saludogral);
